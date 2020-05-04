3e13b0c1ed163f9425323affeb1b3b2cad9e65e0
chaha0s <yashdama@gmail.com>
2020-05-04 15:18:46 +0530

    indexTest.js 3

--- indexTest.js
+++ indexTest.js
@@ -34,6 +34,7 @@ function getHS(){
     var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) { 
+            
         }
   };
       var user=getCookie("username");
