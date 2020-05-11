f204645c259610f15ae42f3634560448890c8532
chaha0s <yashdama@gmail.com>
2020-05-11 15:16:24 +0530

    .

--- indexFr.js
+++ indexFr.js
@@ -30,7 +30,7 @@
       var scoree=window.btoa(highScore);
       var unix = Math.round(+new Date()/1000)+1;
       var unixe=window.btoa(unix);
-      xhttp.open("GET", "online/updateScore.php?email="+ emaile +"&score="+scoree+"&pwd="+unixe, true);
+      xhttp.open("GET", "https://chotapandit.online/v2/updateScore.php?email="+ emaile +"&score="+scoree+"&pwd="+unixe, true);
       xhttp.send();
     }
   
